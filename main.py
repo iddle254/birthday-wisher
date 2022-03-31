1c868f817336e7b36b9e235577bfe02e4e16a6bd
charles <charlesiddle99@gmail.com>
2022-03-31 20:05:02 +0300

    compared dates.csv to todays date and sent mail
    Check if today matches a birthday in the birthdays.csv
    Used dictionary comprehension to create a dictionary from birthday.csv
    Then you could compare and see if today's month/day tuple matches one of the keys in birthday_dict
    If there is a match, pick a random letter from letter_templates and replace the [NAME] with the person's actual name
    Send the letter generated to that person's email address.

--- main.py
+++ main.py
@@ -11,3 +11,24 @@ MY_PASSWORD = "*****"
 today = (dt.datetime.now().month, dt.datetime.now().day)
 
 mails = read_csv("./birthdays.csv")
+
+birthdays_dict = {(row.month, row.day): row for (index, row) in mails.iterrows()}
+if today in birthdays_dict:
+    birthday_person=birthdays_dict[today]
+    file_path = f"letter_templates/letter_{(randint(1,3))}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("Angela", "Moss")
+        contents = contents.replace("[NAME]", birthday_person["name"].title())
+        print(contents)
+
+
+    with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
+        connection.starttls()
+        connection.login(MY_MAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_MAIL,
+            to_addrs=birthday_person['email'],
+            msg=f"Subject:Happy birthday\n\n{contents}"
+
+        )
